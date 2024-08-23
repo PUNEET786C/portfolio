a845ee12c049d93d062061a85cae3ac2eb31b05b
PUNEET786C <165518662+PUNEET786C@users.noreply.github.com>
2024-08-23 14:38:54 +0530

    portfolio

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function scrollToSection(sectionId) {
+    document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
+}
+function toggleMenu() {
+    const navLinks = document.getElementById('nav-links');
+    const menuIcon = document.querySelector('.menu-icon');
+    const logo = document.getElementById('logo');
+
+    navLinks.classList.toggle('active');
+    menuIcon.classList.toggle('active');
+   
+}
